b9c5f95dd488cf896b5332f7eda7e8c65d5e191f
lukaramishvili <1906262+lukaramishvili@users.noreply.github.com>
2013-06-05 09:53:16 +0400

    clear scratch default text

--- haskellito/haskellito.el
+++ haskellito/haskellito.el
@@ -1,9 +1,15 @@
+;;hint: to see emacs lisp function source, use 
+;;M-x find-function RET (function name) RET
+
 ;;open a .hs file in one buffer and GHCi in another
 (defun haskellito-san (hs-file)
-  (interactive "sHaskell File:")
+  (interactive
+   (find-file-read-args "Haskell file: "
+                        (confirm-nonexistent-file-or-buffer)))
+  ;;(haskell-mode)
   (split-window-horizontally)
   (find-file hs-file)
   (other-window 1)
-  (shell)
-  ;open GHCi here
-  )
+  (shell (current-buffer))
+  (process-send-string nil "ghci\n")
+  (process-send-string nil ":set prompt haskellito>\n"))
